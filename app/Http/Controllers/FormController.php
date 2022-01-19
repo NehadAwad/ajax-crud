0f7acab8a099a5f44a2389c437ca2db2d4da2ebc
NehadAwad <nehadakash622@gmail.com>
2022-01-19 23:05:32 +0600

    post method added

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -8,43 +8,10 @@ use Illuminate\Http\Request;
 class FormController extends Controller
 {
 
-    public function index()
+    public function insert(Request $request)
     {
-        //
+        dd($request);
     }
 
 
-    public function create()
-    {
-        //
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-
-    public function show(Form $form)
-    {
-        //
-    }
-
-
-    public function edit(Form $form)
-    {
-        //
-    }
-
-
-    public function update(Request $request, Form $form)
-    {
-        //
-    }
-
-
-    public function destroy(Form $form)
-    {
-        //
-    }
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ajax Crud</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+    <br><br><br><br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <form method="POST">@csrf
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email" class="form-control" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" class="form-control" placeholder="Enter password">
+                    </div>
+                    <button type="submit" class="btn btn-primary" id="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+    <script>
+        $("#submit").click(function (){
+            $.ajax({
+                url: 'formsubmit',
+                type: 'post',
+                data: $('form').serialize(),
+                success: function (result){
+                    alert(result);
+                }
+            });
+        });
+    </script>
+
+</body>
+</html>
