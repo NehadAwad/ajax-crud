b47b65a25b123a8f4a66cf00591e609a9eb09531
NehadAwad <nehadakash622@gmail.com>
2022-01-19 22:29:18 +0600

    form mig added

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Form;
+use Illuminate\Http\Request;
+
+class FormController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Form $form)
+    {
+        //
+    }
+
+
+    public function edit(Form $form)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Form $form)
+    {
+        //
+    }
+
+
+    public function destroy(Form $form)
+    {
+        //
+    }
+}
